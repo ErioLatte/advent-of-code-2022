87f4dfd36df58bf766613efaff83b905b4b47195
erio_y <erioyoshino99@gmail.com>
2023-05-23 17:48:59 +0700

    Update day2.py

--- day2.py
+++ day2.py
@@ -15,14 +15,17 @@ ROCK  --  PAPER  --  SCISSOR  --  ROCK  --  PAPER
 
 see the pattern?
 """
-def calculate1(p1, p2):
+def calculate1(p2, p1):
     if (p1 + 1) % 3 == p2: # lose
-        return p1
+        return p1 + 1
     elif p1 == p2: # tie
-        return 3 + p1
+        return 3 + p1 + 1
     else: # win
-        return 6 + p1
+        return 6 + p1 + 1
 
+"""
+calculation method by @HotPotatoC, he use a matrix to calculate. i think its a great approach for this 
+"""
 def calculate(p2, p1):
     matrices = [
         [3, 0, 6],
@@ -33,6 +36,14 @@ def calculate(p2, p1):
     # print(value)
     return value
 
+def part2(p2):
+    if(p2==0):
+        return 1
+    elif(p2==1):
+        return 0
+    else:
+        return 2
+
 def main():
     a = 0
     b = 0
@@ -40,7 +51,7 @@ def main():
     with open("input_d2.txt", "r") as f:
         for line in f:
             a = line.split()
-            ans += calculate(ord(a[0]) - ord('A'), ord(a[1])-ord('X'))
+            ans += calculate1(ord(a[0]) - ord('A'), part2(ord(a[0]) - ord('A')))
         print(ans)
 
 
